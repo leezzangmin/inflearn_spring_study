7f3faa2ab8ce184df9461070843e7095c6db27fa
leezzangmin <ckdals12345678@gmail.com>
2022-02-28 00:19:41 +0900

    Service 에 @autowired 추가

--- hello/src/main/java/hello/hellospring/service/MemberService.java
+++ hello/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,6 +3,7 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,8 +11,12 @@ import java.util.Optional;
 
 @Service
 public class MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-
+    private final MemberRepository memberRepository;
+    
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
     public Long join(Member member) {
         Optional<Member> result = memberRepository.findByName(member.getName());
         result.ifPresent(m ->{
