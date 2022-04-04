3a369094b51ea6ce7532a7122618632bd436fb9a
leezzangmin <ckdals12345678@gmail.com>
2022-04-04 23:04:05 +0900

    검증 errors 생성로직 + model에 추가

--- mvc2_validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ mvc2_validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -5,10 +5,13 @@ import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/validation/v1/items")
@@ -38,7 +41,33 @@ public class ValidationItemControllerV1 {
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
+
+        //검증오류 보관
+        Map<String, String> errors= new HashMap<>();
+        if (!StringUtils.hasText(item.getItemName())) {
+            errors.put("itemName", "상품이름 필수");
+        }
+        if (item.getPrice() == null || item.getPrice()<1000 || item.getPrice() >1000000) {
+            errors.put("price","가격은 1000~1000000");
+        }
+        if (item.getQuantity()==null || item.getQuantity()>=9999) {
+            errors.put("quantity","수량은 최대 9999");
+        }
+        if (item.getPrice()!=null && item.getQuantity()!=null ) {
+            int result = item.getPrice() * item.getQuantity();
+            if (result<10000) {
+                errors.put("global","가격x수량 은 10000원 이상"+String.valueOf(result));
+            }
+        }
+
+        if (!errors.isEmpty()) {
+            model.addAttribute("errors",errors);
+            return "validation/v1/addForm";
+        }
+
+
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
