0403d1ce37bfffbd556048156603765faec2562a
leezzangmin <ckdals12345678@gmail.com>
2022-04-07 23:16:12 +0900

    Bean validation 컨트롤러 버전2 맛보기

--- mvc2_validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ mvc2_validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,11 +1,13 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
 public class Item {
 
     private Long id;
+    @NonNull
     private String itemName;
     private Integer price;
     private Integer quantity;
--- mvc2_validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ mvc2_validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -7,6 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -42,7 +44,7 @@ public class ValidationItemControllerV1 {
         return "validation/v1/addForm";
     }
 
-    @PostMapping("/add")
+   // @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
 
         //검증오류 보관
@@ -76,6 +78,24 @@ public class ValidationItemControllerV1 {
         redirectAttributes.addAttribute("status", true);
         return "redirect:/validation/v1/items/{itemId}";
     }
+
+    @PostMapping("/add")
+    public String addItem2(@Validated @RequestBody Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "validation/v1/addForm";
+        }
+
+
+
+
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v1/items/{itemId}";
+    }
+
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
