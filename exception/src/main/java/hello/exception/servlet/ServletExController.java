f06fb3f07f31b91b5d1474e60ede05c063b5ddba
leezzangmin <ckdals12345678@gmail.com>
2022-04-18 22:46:13 +0900

    에러페이지 설정

--- exception/src/main/java/hello/exception/servlet/ServletExController.java
+++ exception/src/main/java/hello/exception/servlet/ServletExController.java
@@ -1,5 +1,7 @@
 package hello.exception.servlet;
 
+import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,11 @@ public class ServletExController {
     @GetMapping("/error-ex")
     public void errorEx() {
         throw new RuntimeException("예외1");
+    }
+    
+    @GetMapping("/error-404")
+    public void error404(HttpServletResponse r) throws IOException { //IOException은 메서드에 필요한 것...?
+        r.sendError(404,"404오류");
     }
 
 }
