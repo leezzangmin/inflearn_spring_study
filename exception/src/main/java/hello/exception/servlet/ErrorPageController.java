1281c53fb7c2299a941ad0f54995482d34772ce2
leezzangmin <ckdals12345678@gmail.com>
2022-04-21 00:09:01 +0900

    HandlerExceptionResolver 적용

--- exception/src/main/java/hello/exception/servlet/ErrorPageController.java
+++ exception/src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,8 +1,15 @@
 package hello.exception.servlet;
 
+import static javax.servlet.RequestDispatcher.ERROR_EXCEPTION;
+import static javax.servlet.RequestDispatcher.ERROR_STATUS_CODE;
+
+import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,6 +21,19 @@ public class ErrorPageController {
     public String error404(HttpServletRequest request, HttpServletResponse response) {
         log.info("error 404");
         return "error-page/404";
+    }
+
+    @RequestMapping(value="/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> errorPage500Api(
+        HttpServletRequest request, HttpServletResponse response) {
+
+        log.info("API_ERROR_500");
+        Map<String,Object> result = new HashMap<>();
+        Exception ex= (Exception) request.getAttribute(ERROR_EXCEPTION);
+        result.put("status", request.getAttribute(ERROR_STATUS_CODE));
+        result.put("message", ex.getMessage());
+
+        return ResponseEntity.ok(result);
     }
 
     @RequestMapping("/error-page/500")
