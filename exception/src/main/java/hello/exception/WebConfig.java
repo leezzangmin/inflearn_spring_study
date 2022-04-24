f40907a93225ebef2f0dfa9c06083fe492d41e21
leezzangmin <ckdals12345678@gmail.com>
2022-04-24 23:43:03 +0900

    jpa 실습

--- exception/src/main/java/hello/exception/WebConfig.java
+++ exception/src/main/java/hello/exception/WebConfig.java
@@ -14,7 +14,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-
     @Override
     public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
         resolvers.add(new MyHandlerExceptionResolver());
---
+++ jpa1/src/main/java/inflearn/RoleType.java
@@ -0,0 +1,5 @@
+package inflearn;
+
+public enum RoleType {
+    USER, ADMIN
+}
