914977b642e674cdae5eb5559a2660b99b40e1d5
leezzangmin <ckdals12345678@gmail.com>
2022-05-15 01:34:10 +0900

    빈스코프

--- practice/src/main/java/zzangmin/practice/service/TempService.java
+++ practice/src/main/java/zzangmin/practice/service/TempService.java
@@ -1,9 +1,11 @@
 package zzangmin.practice.service;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope(value = "prototype")
 public class TempService {
 
     public void save() {
